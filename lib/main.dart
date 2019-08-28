80b8d2b0b57bdd28341bda02c18299b88f609ba8
jonathan <jr111_@hotmail.com>
2019-08-28 17:52:34 -0500

    Create listview and listtile

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/home_temp.dart';
+
+    void main() => runApp(MyApp());
+
+  class MyApp extends StatelessWidget {
+
+    @override
+    Widget build(BuildContext Context){
+      return MaterialApp(
+        title: 'Componentes Flutter',
+        debugShowCheckedModeBanner: false,
+        home: HomePageTemp()
+      );
+    }
+  }
+
